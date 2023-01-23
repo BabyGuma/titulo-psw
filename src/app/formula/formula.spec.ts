2711627eadfce1277bf065bf1471b3983f7e7050
Angel Guarneros <111380789+BabyGuma@users.noreply.github.com>
2023-01-23 13:36:28 -0600

    Release (#10)
    * Update CI.yml
    
    * Update CI.yml
    
    * Develop (#8)
    
    * Create testing (#5)
    
    * Update (#6)
    
    * Update
    
    * Update
    
    * Update integration-testing to develop #3 (#7)
    
    * Update
    
    * Update
    
    * Update file spec.ts #2
    
    * Update spec.ts #4
    
    * Update #2
    
    * changes #4

--- src/app/formula/formula.spec.ts
+++ src/app/formula/formula.spec.ts
@@ -5,24 +5,24 @@ describe ('funcion', () =>{
         const area = ecuacion(9.5, 3.5, 4);
         expect(area).toBe(26);
     })
-})
-describe ('funcion', () =>{
+
+
+
     it('Should return area = 28, if a= 8 and b = 6 and h = 4', ()=> {
         const area = ecuacion(8, 6, 4);
         expect(area).toBe(28);
     })
 
-describe ('funcion', () =>{
-        it('Should return area = 18, if a= 6 and b = 3 and h = 4', ()=> {
-            const area = ecuacion(6, 3, 4);
-            expect(area).toBe(18);
-        })
 
-describe ('funcion', () =>{
-            it('Should return area = 21, if a= 8 and b = 6 and h = 3', ()=> {
-                const area = ecuacion(8, 6, 3);
-                expect(area).toBe(21);
-            })
-        })
+    it('Should return area = 18, if a= 6 and b = 3 and h = 4', ()=> {
+        const area = ecuacion(6, 3, 4);
+        expect(area).toBe(18);
+    })
+
+    it('Should return area = 21, if a= 8 and b = 6 and h = 3', ()=> {
+        const area = ecuacion(8, 6, 3);
+        expect(area).toBe(21);
+
+
     })
 })
